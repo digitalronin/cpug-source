9a6e717cfc68f680d1103de0117df62837161f8b
David Salgado <david@digitalronin.com>
2020-07-03 16:39:20 +0100

    Update publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-echo "Hello from the publish script"
-ls -l docs
+REPOSITORY_PATH="https://${GITHUB_ACCESS_TOKEN}@github.com/$GITHUB_PAGES_REPO_AUTHOR/$GITHUB_PAGES_REPO_NAME.git"
+
+echo "Checkout the publish repo"
+git clone $REPOSITORY_PATH
+
+echo "Add compiled files"
+cp -R docs/* $GITHUB_PAGES_REPO_NAME
+
+echo "Push the changes"
+cd $GITHUB_PAGES_REPO_NAME
+git add .
+git commit -m "Published at $(date)"
+git push
+
+echo "Publishing complete"
