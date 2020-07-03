d991b1c1c1a23a33507946e6e7174beaf871d8fc
David Salgado <david@digitalronin.com>
2020-07-03 16:51:20 +0100

    Try using different github token

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-REPOSITORY_PATH="https://${GITHUB_ACCESS_TOKEN}@github.com/$GITHUB_PAGES_REPO_AUTHOR/$GITHUB_PAGES_REPO_NAME.git"
+REPOSITORY_PATH="https://${PUBLISHING_GIT_TOKEN}@github.com/$GITHUB_PAGES_REPO_AUTHOR/$GITHUB_PAGES_REPO_NAME.git"
 
 echo "Checkout the publish repo"
 git clone $REPOSITORY_PATH
