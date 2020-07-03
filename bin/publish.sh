457787b6b03843d7e0ab7df9fbb4b49e83a84ec8
David Salgado <david@digitalronin.com>
2020-07-03 16:57:14 +0100

    Add non-root publishing hack

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,9 +8,20 @@ git clone $REPOSITORY_PATH
 echo "Add compiled files"
 cp -R docs/* $GITHUB_PAGES_REPO_NAME
 
-echo "Push the changes"
 cd $GITHUB_PAGES_REPO_NAME
 
+# this is only required while developing, when the site is hosted below the root
+# of the relevant domain (in my case, at https://digitalronin.github.io/cpug-publish)
+echo "Hack for non-root publishing"
+find . -name '*.html' | xargs sed -i '' 's/stylesheets.manifest.css/$(PUBLISH_REPO)\/stylesheets\/manifest.css/'; \
+find . -name '*.html' | xargs sed -i '' 's/javascripts.application.js/$(PUBLISH_REPO)\/javascripts\/application.js/'; \
+find . -name '*.html' | xargs sed -i '' 's/assets/$(PUBLISH_REPO)\/assets/'; \
+find . -name '*.html' | xargs sed -i '' 's/"\/images/"\/$(PUBLISH_REPO)\/images/'; \
+find . -name '*.css' | xargs sed -i '' 's/"\/images/"\/$(PUBLISH_REPO)\/images/'; \
+find . -name '*.css' | xargs sed -i '' 's/assets/$(PUBLISH_REPO)\/assets/'; \
+find . -name '*.js' | xargs sed -i '' 's/"\/search.json/"\/$(PUBLISH_REPO)\/search.json/'; \
+
+echo "Push the changes"
 git config --global user.email "foo@bar.com"
 git config --global user.name "Foo Bar"
 git add .
