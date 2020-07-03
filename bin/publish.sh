dd4ead8703e4eade0f575dd45caf89d63449fb1f
David Salgado <david@digitalronin.com>
2020-07-03 17:03:37 +0100

    fix bin/publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -13,13 +13,14 @@ cd $GITHUB_PAGES_REPO_NAME
 # this is only required while developing, when the site is hosted below the root
 # of the relevant domain (in my case, at https://digitalronin.github.io/cpug-publish)
 echo "Hack for non-root publishing"
-find . -name '*.html' | xargs sed -i '' 's/stylesheets.manifest.css/$(PUBLISH_REPO)\/stylesheets\/manifest.css/'; \
-find . -name '*.html' | xargs sed -i '' 's/javascripts.application.js/$(PUBLISH_REPO)\/javascripts\/application.js/'; \
-find . -name '*.html' | xargs sed -i '' 's/assets/$(PUBLISH_REPO)\/assets/'; \
-find . -name '*.html' | xargs sed -i '' 's/"\/images/"\/$(PUBLISH_REPO)\/images/'; \
-find . -name '*.css' | xargs sed -i '' 's/"\/images/"\/$(PUBLISH_REPO)\/images/'; \
-find . -name '*.css' | xargs sed -i '' 's/assets/$(PUBLISH_REPO)\/assets/'; \
-find . -name '*.js' | xargs sed -i '' 's/"\/search.json/"\/$(PUBLISH_REPO)\/search.json/'; \
+PUBLISH_REPO="cpug-publish"
+find . -name '*.html' | xargs sed -i "s/stylesheets.manifest.css/${PUBLISH_REPO}\/stylesheets\/manifest.css/"
+find . -name '*.html' | xargs sed -i "s/javascripts.application.js/${PUBLISH_REPO}\/javascripts\/application.js/"
+find . -name '*.html' | xargs sed -i "s/assets/${PUBLISH_REPO}\/assets/"
+find . -name '*.html' | xargs sed -i "s/\"\/images/\"\/${PUBLISH_REPO}\/images/"
+find . -name '*.css' | xargs sed -i "s/\"\/images/\"\/${PUBLISH_REPO}\/images/"
+find . -name '*.css' | xargs sed -i "s/assets/${PUBLISH_REPO}\/assets/"
+find . -name '*.js' | xargs sed -i "s/\"\/search.json/\"\/${PUBLISH_REPO}\/search.json/"
 
 echo "Push the changes"
 git config --global user.email "foo@bar.com"
